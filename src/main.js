61bac576ae9dc6f87fbe82b0a1a91be591bd43e9
Engelbert Niehaus <niehausbert@gmail.com>
2020-11-24 12:17:24 +0100

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,181 @@
+var languages = [
+    {
+        "text": "English",
+        "value": "en"
+    },
+    {
+        "text": "Spanish",
+        "value": "es"
+    },
+    {
+        "text": "German",
+        "value": "de"
+    },
+    {
+        "text": "French",
+        "value": "fr"
+    },
+    {
+        "text": "Italian",
+        "value": "it"
+    },
+    {
+        "text": "Dutch",
+        "value": "nl"
+    },
+    {
+        "text": "Japanese",
+        "value": "ja"
+    },
+    {
+        "text": "Polish",
+        "value": "pl"
+    },
+    {
+        "text": "Russian",
+        "value": "ru"
+    },
+    {
+        "text": "Swedish",
+        "value": "sv"
+    },
+    {
+        "text": "Vietnamese",
+        "value": "vi"
+    },
+    {
+        "text": "Arabic",
+        "value": "ar"
+    },
+    {
+        "text": "Indonesian",
+        "value": "id"
+    },
+    {
+        "text": "Malay",
+        "value": "ms"
+    },
+    {
+        "text": "Catalan",
+        "value": "ca"
+    },
+    {
+        "text": "Czech",
+        "value": "cs"
+    },
+    {
+        "text": "Basque",
+        "value": "eu"
+    },
+    {
+        "text": "Persian",
+        "value": "fa"
+    },
+    {
+        "text": "Korean",
+        "value": "ko"
+    },
+    {
+        "text": "Hungarian",
+        "value": "hu"
+    },
+    {
+        "text": "Norwegian",
+        "value": "no"
+    },
+    {
+        "text": "Portuguese",
+        "value": "pt"
+    },
+    {
+        "text": "Romanian",
+        "value": "ro"
+    },
+    {
+        "text": "Serbian",
+        "value": "sr"
+    },
+    {
+        "text": "Serbo-Croatian",
+        "value": "sh"
+    },
+    {
+        "text": "Finnish",
+        "value": "fi"
+    },
+    {
+        "text": "Turkish",
+        "value": "tr"
+    },
+    {
+        "text": "Ukrainian",
+        "value": "uk"
+    },
+    {
+        "text": "Chinese",
+        "value": "zh"
+    },
+    {
+        "text": "Bosnian",
+        "value": "bs"
+    },
+    {
+        "text": "Bulgarian",
+        "value": "bg"
+    },
+    {
+        "text": "Danish",
+        "value": "da"
+    },
+    {
+        "text": "Estonian",
+        "value": "et"
+    },
+    {
+        "text": "Greek",
+        "value": "el"
+    },
+    {
+        "text": "Esperanto",
+        "value": "eo"
+    },
+    {
+        "text": "Galician",
+        "value": "gl"
+    },
+    {
+        "text": "Hebrew",
+        "value": "he"
+    },
+    {
+        "text": "Croatian",
+        "value": "hr"
+    },
+    {
+        "text": "Latvian",
+        "value": "lv"
+    },
+    {
+        "text": "Lithuanian",
+        "value": "lt"
+    },
+    {
+        "text": "Norwegian Nynorsk",
+        "value": "nn"
+    },
+    {
+        "text": "Slovak",
+        "value": "sk"
+    },
+    {
+        "text": "Slovenian",
+        "value": "sl"
+    },
+    {
+        "text": "Thai",
+        "value": "th"
+    }
+]
+module.exports = {
+   "languages": languages
+};
